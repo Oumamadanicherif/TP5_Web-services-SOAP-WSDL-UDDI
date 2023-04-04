0564d542a21564415668479a62a0845083aed0d8
Oumaima Madani-Cherif <94255889+Oumamadanicherif@users.noreply.github.com>
2023-04-04 22:38:29 +0000

    Delete Desktop/Java Jee/jpa-appUseCase2 directory

--- Desktop/Java Jee/jpa-appUseCase2/src/main/java/ma/enset/jpaappusecase2/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.enset.jpaappusecase2.repositories;
-
-import ma.enset.jpaappusecase2.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String>  {
-}
